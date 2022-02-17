fb680b42516d2065c985d0e65ec1f42b3f909c53
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-02-17 20:47:41 +0000

    finishes error handling exercise

--- Code_Exercises/Exercise_04_Handling_Errors/solution.cpp
+++ Code_Exercises/Exercise_04_Handling_Errors/solution.cpp
@@ -1,56 +1,47 @@
-/*
- SYCL Academy (c)
-
- SYCL Academy is licensed under a Creative Commons
- Attribution-ShareAlike 4.0 International License.
-
- You should have received a copy of the license along with this
- work.  If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
-*/
-
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
-#if __has_include(<SYCL/sycl.hpp>)
-#include <SYCL/sycl.hpp>
-#else
 #include <CL/sycl.hpp>
-#endif
 
 class scalar_add;
 
-TEST_CASE("handling_errors", "handling_errors_source") {
-  int a = 18, b = 24, r = 0;
+int main() {
+    int a { 18 };
+    int b { 24 };
+    int r { 0 };
+    std::cout << "The value of the result buffer before the kernel execution is: " << r << '\n';
 
-  try {
-    auto asyncHandler = [&](sycl::exception_list exceptionList) {
-      for (auto& e : exceptionList) {
-        std::rethrow_exception(e);
-      }
-    };
+    try {
+        auto asyncHandler = [&](sycl::exception_list exceptionList)
+        {
+            for (auto& e : exceptionList) {
+                std::rethrow_exception(e);
+            }
+        };
 
-    auto defaultQueue = sycl::queue{asyncHandler};
+        auto bufA { sycl::buffer { &a, sycl::range { 1 } } };
+        auto bufB { sycl::buffer { &b, sycl::range { 1 } } };
+        auto bufR { sycl::buffer { &r, sycl::range { 1 } } };
 
-    {
-      auto bufA = sycl::buffer{&a, sycl::range{1}};
-      auto bufB = sycl::buffer{&b, sycl::range{1}};
-      auto bufR = sycl::buffer{&r, sycl::range{1}};
+        // auto defaultQueue { sycl::queue() };
+        auto defaultQueue { sycl::queue { asyncHandler } };
+        defaultQueue
+            .submit(
+                [&](sycl::handler& cgh)
+                {
+                    auto accA { sycl::accessor { bufA, cgh, sycl::read_only } };
+                    auto accB { sycl::accessor { bufB, cgh, sycl::read_only } };
+                    auto accR { sycl::accessor { bufR, cgh, sycl::write_only } };
 
-      defaultQueue
-          .submit([&](sycl::handler& cgh) {
-            auto accA = sycl::accessor{bufA, cgh, sycl::read_only};
-            auto accB = sycl::accessor{bufB, cgh, sycl::read_only};
-            auto accR = sycl::accessor{bufR, cgh, sycl::write_only};
+                    cgh.single_task<scalar_add>([=]() { accR[0] = accA[0] + accB[0]; });
+                })
+            .wait();
 
-            cgh.single_task<scalar_add>([=]() { accR[0] = accA[0] + accB[0]; });
-          })
-          .wait();
+        defaultQueue.throw_asynchronous();
+    } catch (const sycl::exception& e) {
+        std::cout << "SYCL asynchronous exception caught: " << e.what() << '\n';
+    } catch (...) {
+        std::cout << "SYCL synchronous exception caught!" << '\n';
     }
 
-    defaultQueue.throw_asynchronous();
-  } catch (const sycl::exception& e) {
-    std::cout << "Exception caught: " << e.what() << std::endl;
-  }
+    std::cout << "The value of the result buffer after kernel execution is: " << r << '\n';
 
-  REQUIRE(r == 42);
+    return 0;
 }
