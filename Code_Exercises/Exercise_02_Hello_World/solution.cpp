5d27704cc01f2776e649de1552aed1a8a064a2e6
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-02-17 15:29:18 +0000

    finished exercise 02 hello world

--- Code_Exercises/Exercise_02_Hello_World/solution.cpp
+++ Code_Exercises/Exercise_02_Hello_World/solution.cpp
@@ -1,34 +1,35 @@
-/*
- SYCL Academy (c)
-
- SYCL Academy is licensed under a Creative Commons
- Attribution-ShareAlike 4.0 International License.
-
- You should have received a copy of the license along with this
- work.  If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
-*/
-
 #define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
-#if __has_include(<SYCL/sycl.hpp>)
-#include <SYCL/sycl.hpp>
-#else
 #include <CL/sycl.hpp>
-#endif
+// #include <catch2/catch.hpp>
 
 class hello_world;
 
-TEST_CASE("hello_world", "hello_world_solution") {
-  auto defaultQueue = sycl::queue{};
+int main() {
+    // class hello_world; // show what happens when defined as a local class
 
-  defaultQueue
-      .submit([&](sycl::handler& cgh) {
-        auto os = sycl::stream{128, 128, cgh};
+    auto defaultQueue { sycl::queue {} };
 
-        cgh.single_task<hello_world>([=]() { os << "Hello World!\n"; });
-      })
-      .wait();
+    defaultQueue
+        .submit(
+            [&](sycl::handler& cgh)
+            {
+                auto outputStream { sycl::stream { 128, 128, cgh } };
 
-  REQUIRE(true);
+                cgh.single_task<hello_world>([=]() { outputStream << "Hello World!\n"; });
+            })
+        .wait();
+
+    return 0;
 }
+
+// TEST_CASE("hello_world", "hello_world_solution") {
+// auto defaultQueue = sycl::queue {};
+
+// defaultQueue.submit([&](sycl::handler& cgh)
+//         {
+//             auto os = sycl::stream { 128, 128, cgh };
+//
+//             cgh.single_task<hello_world>([=]() { os << "Hello World!\n"; });
+//         }).wait();
+
+// }
